c18e3ce3084d543f8d92e5057a4e310078e7d793
James Wickett <wickett@gmail.com>
2013-06-18 12:28:13 -0700

    addoing test kitchen

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'james@wickett.me'
 license          'All rights reserved'
 description      'Installs/Configures wickett-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
